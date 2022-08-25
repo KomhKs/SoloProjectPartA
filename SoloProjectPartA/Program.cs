c68620e5b7f83de6403c250cafd89eec7770825d
KomhKs <m.n.kollaros@gmail.com>
2022-08-25 10:20:55 +0200

    Added Console.Writeline

---
+++ SoloProjectPartA/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloProjectPartA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You did it! You crazy son of a bitch you did it!");
+        }
+    }
+}
