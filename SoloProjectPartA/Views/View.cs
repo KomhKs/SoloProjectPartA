1440f283156a7eb925b534ee35563401790f0ea5
KomhKs <m.n.kollaros@gmail.com>
2022-09-03 19:13:26 +0200

    corrected DbSe to list and added .ToList()

--- SoloProjectPartA/Views/View.cs
+++ SoloProjectPartA/Views/View.cs
@@ -11,7 +11,7 @@ namespace SoloProjectPartA.Views
 {
     public class View
     {
-        public static void ShowAllStudents(DbSet<Student> students, string message)
+        public static void ShowAllStudents(List<Student> students, string message)
         {
             Console.WriteLine(message);
             Console.WriteLine($"{"Student Id",-15}{"FirstName",-25}{"LastName",-25}{"DateOfBirth",-25}{"TuitionFees"}");
@@ -20,7 +20,7 @@ namespace SoloProjectPartA.Views
                 Print.PrintAllStudents(stu);
             }
         }
-        public static void ShowAllTrainers(DbSet<Trainer> trainers, string message)
+        public static void ShowAllTrainers(List<Trainer> trainers, string message)
         {
             Console.WriteLine(message);
             Console.WriteLine($"{"Trainer Id",-15}{"FirstName",-25}{"LastName",-25}{"Subject",-25}");
@@ -29,7 +29,7 @@ namespace SoloProjectPartA.Views
                 Print.PrintAllTrainers(tr);
             }
         }
-        public static void ShowAllCourses(DbSet<Course> courses, string message)
+        public static void ShowAllCourses(List<Course> courses, string message)
         {
             Console.WriteLine(message);
             Console.WriteLine($"{"Course Id",-15}{"Title",-25}{"Stream",-30}{"Type",-25}{"StartDate",-15}{"EndDate"}");
@@ -38,7 +38,7 @@ namespace SoloProjectPartA.Views
                 Print.PrintAllCourses(cou);
             }
         }
-        public static void ShowAllAssignments(DbSet<Assignment> assignments, string message)
+        public static void ShowAllAssignments(List<Assignment> assignments, string message)
         {
             Console.WriteLine(message);
             Console.WriteLine($"{"Assignment Id",-15}{"Title",-25}{"Description",-25}{"SubDateTime",-25}{"OralMark",-25}{"TotalMark",-25}");
